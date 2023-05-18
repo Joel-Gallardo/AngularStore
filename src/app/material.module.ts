51ebd7daa0751441bc04575e6224d0a791753ec6
Joel Gallardo Manjarrez <joel.gallardo@coppel.com>
2023-05-18 08:52:26 -0700

    communication from father component to child with @input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-    exports: [MatToolbarModule, MatIconModule, MatCardModule]
+    exports: [
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatButtonModule
+    ],
 })
-
-export class MaterialModule{
-
-}
+export class MaterialModule { }
